772b476d02ce4e7a9ddf9b24a838629eaf2a0837
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-03-08 12:00:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,4 +21,7 @@ function sayHiToGrandma(string) {
   else if (string.toUpperCase() === string) {
     return "YES INDEED!";
   }
+  else if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  }
 }
